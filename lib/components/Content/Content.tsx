7c83da1c546b26fbfb29e3e7994dcb4d2537b013
Wojciech Piątkowski <chmurson@gmail.com>
2025-08-26 15:32:56 +0200

    Align sidebar with background (#26)

---
+++ lib/components/Content/Content.tsx
@@ -0,0 +1,6 @@
+import { cn } from "@/utils";
+import type { FC, PropsWithChildren } from "react";
+
+export const Content: FC<PropsWithChildren<{ className?: string }>> = ({ children, className }) => {
+  return <div className={cn("box-border flex-1 w-full relative overflow-hidden bg-sidebar border-l-2 border-l-white dark:border-l-1 dark:border-l-[#353535]", className)}>{children}</div>;
+};
--- lib/components/DemoLayout.tsx
+++ lib/components/DemoLayout.tsx
@@ -1,4 +1,5 @@
 import { AppsSidebar } from "./AppsSidebar";
+import { Content } from "./Content";
 import { Header } from "./Header";
 import Toolname from "@/assets/tool-name.svg";
 
@@ -6,9 +7,9 @@ export const DemoLayout = () => {
   return (
     <div>
       <Header endSlot={<div className="text-gray-100 mr-2">End slot content</div>} toolNameSrc={Toolname} />
-      <div className="flex h-full w-full items-center justify-center">
+      <div className="flex h-full w-full items-stretch justify-center">
         <AppsSidebar activeLink="debugger" />
-        <div className="w-full h-full"></div>
+        <Content />
       </div>
     </div>
   );
