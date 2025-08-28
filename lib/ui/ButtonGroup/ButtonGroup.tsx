f7516061000686b0bb64e335a01c5e5cc75e7790
Wojciech Piątkowski <chmurson@gmail.com>
2025-08-28 17:01:34 +0200

    feat: button group

---
+++ lib/ui/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { cn } from "@/utils";
+import styles from "./ButtonGroup.module.css";
+
+export interface ButtonGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+  /**
+   * The orientation of the button group
+   * @default "horizontal"
+   */
+  orientation?: "horizontal" | "vertical";
+}
+
+export const ButtonGroup = React.forwardRef<HTMLDivElement, ButtonGroupProps>(
+  ({ className, orientation = "horizontal", children, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          styles['buttonGroup'],
+          orientation === "horizontal" ? styles['horizontal'] : styles['vertical'],
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+ButtonGroup.displayName = "ButtonGroup";
