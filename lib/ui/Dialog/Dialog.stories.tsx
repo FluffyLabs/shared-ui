6b3e1d63df2084b5c3c1fb2a7910e413d8d85255
Wojciech Piątkowski <chmurson@gmail.com>
2025-10-03 14:51:32 +0200

    feat: add small/medium and branded/normal variants

--- lib/ui/Dialog/Dialog.stories.tsx
+++ lib/ui/Dialog/Dialog.stories.tsx
@@ -5,11 +5,11 @@ import { Button } from "../Button";
 import Dialog from "./Dialog";
 
 const meta = {
-  title: "UI/Dialog",
-  component: Dialog,
-  parameters: {
-    layout: "fullscreen",
-  },
+	title: "UI/Dialog",
+	component: Dialog,
+	parameters: {
+		layout: "fullscreen",
+	},
 } satisfies Meta<typeof Dialog>;
 
 export default meta;
@@ -17,16 +17,16 @@ export default meta;
 type Story = StoryObj<typeof Dialog>;
 
 const ThemeSwitcherDecorator = (Story: React.FC) => (
-  <div className="p-4 bg-card">
-    <div className="flex justify-between items-center mb-4 gap-2">
-      <h3 className="text-md font-semibold text-foreground">Theme Switcher</h3>
-      <ToggleDarkModeIcon />
-    </div>
-    <div className="flex flex-col gap-2 items-start">
-      <h2 className="text-lg text-foreground">Story</h2>
-      <Story />
-    </div>
-  </div>
+	<div className="p-4 bg-card">
+		<div className="flex justify-between items-center mb-4 gap-2">
+			<h3 className="text-md font-semibold text-foreground">Theme Switcher</h3>
+			<ToggleDarkModeIcon />
+		</div>
+		<div className="flex flex-col gap-2 items-start">
+			<h2 className="text-lg text-foreground">Story</h2>
+			<Story />
+		</div>
+	</div>
 );
 
 const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
@@ -44,65 +44,103 @@ const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
 					Maecenas adipiscing ante non diam sodales hendrerit.`;
 
 export const Default: Story = {
-  decorators: [ThemeSwitcherDecorator],
-  render: () => (
-    <div className="bg-accent p-12 max-sm:p-6">
-      <Dialog>
-        <Dialog.Header>Start with an example program or upload your file</Dialog.Header>
-        <Dialog.Content>{loremIpsum}</Dialog.Content>
-        <Dialog.Footer>
-          <Button variant="secondary">Secondary</Button>
-        </Dialog.Footer>
-      </Dialog>
-    </div>
-  ),
+	decorators: [ThemeSwitcherDecorator],
+	render: () => (
+		<div className="bg-accent p-12 max-sm:p-6">
+			<Dialog>
+				<Dialog.Header>
+					Start with an example program or upload your file
+				</Dialog.Header>
+				<Dialog.Content>{loremIpsum}</Dialog.Content>
+				<Dialog.Footer>
+					<Button variant="secondary">Secondary</Button>
+				</Dialog.Footer>
+			</Dialog>
+		</div>
+	),
+};
+
+export const DifferentHeaderSizes: Story = {
+	decorators: [ThemeSwitcherDecorator],
+	render: () => (
+		<div className="bg-accent p-12 max-sm:p-6 flex flex-col gap-12">
+			<Dialog>
+				<Dialog.Header size="md" variant="normal">
+					Medium & normal variant
+				</Dialog.Header>
+				<Dialog.Content>{loremIpsum.slice(0, 23)}</Dialog.Content>
+			</Dialog>
+			<Dialog>
+				<Dialog.Header size="md" variant="brand">
+					Medium & branded variant
+				</Dialog.Header>
+				<Dialog.Content>{loremIpsum.slice(0, 23)}</Dialog.Content>
+			</Dialog>
+			<Dialog>
+				<Dialog.Header size="sm" variant="normal">
+					Small & normal variant
+				</Dialog.Header>
+				<Dialog.Content>{loremIpsum.slice(0, 23)}</Dialog.Content>
+			</Dialog>
+			<Dialog>
+				<Dialog.Header size="sm" variant="brand">
+					Small & branded variant
+				</Dialog.Header>
+				<Dialog.Content>{loremIpsum.slice(0, 23)}</Dialog.Content>
+			</Dialog>
+		</div>
+	),
 };
 
 export const RadixSlotExample: Story = {
-  decorators: [ThemeSwitcherDecorator],
-  name: "Mix of Slot and className prop usage",
-  render: () => (
-    <div className="bg-accent p-12 max-sm:p-6">
-      <Dialog asChild>
-        <div className="rounded-none">
-          <Dialog.Header className="rounded-none">Start with an example program or upload your file</Dialog.Header>
-          <Dialog.Content asChild>
-            <div className="text-red-900">{loremIpsum}</div>
-          </Dialog.Content>
-          <Dialog.Footer>
-            <Button variant="secondary">Secondary</Button>
-          </Dialog.Footer>
-        </div>
-      </Dialog>
-    </div>
-  ),
+	decorators: [ThemeSwitcherDecorator],
+	name: "Mix of Slot and className prop usage",
+	render: () => (
+		<div className="bg-accent p-12 max-sm:p-6">
+			<Dialog asChild>
+				<div className="rounded-none">
+					<Dialog.Header className="rounded-none">
+						Start with an example program or upload your file
+					</Dialog.Header>
+					<Dialog.Content asChild>
+						<div className="text-red-900">{loremIpsum}</div>
+					</Dialog.Content>
+					<Dialog.Footer>
+						<Button variant="secondary">Secondary</Button>
+					</Dialog.Footer>
+				</div>
+			</Dialog>
+		</div>
+	),
 };
 
 export const NoFooterSeparator: Story = {
-  decorators: [ThemeSwitcherDecorator],
-  render: () => (
-    <div className="bg-accent p-12 max-sm:p-6">
-      <Dialog>
-        <Dialog.Header>Start with an example program or upload your file</Dialog.Header>
-        <Dialog.Content>{loremIpsum}</Dialog.Content>
-        <Dialog.Footer noSeparator>
-          <Button variant="secondary">Secondary</Button>
-        </Dialog.Footer>
-      </Dialog>
-    </div>
-  ),
+	decorators: [ThemeSwitcherDecorator],
+	render: () => (
+		<div className="bg-accent p-12 max-sm:p-6">
+			<Dialog>
+				<Dialog.Header>
+					Start with an example program or upload your file
+				</Dialog.Header>
+				<Dialog.Content>{loremIpsum}</Dialog.Content>
+				<Dialog.Footer noSeparator>
+					<Button variant="secondary">Secondary</Button>
+				</Dialog.Footer>
+			</Dialog>
+		</div>
+	),
 };
 
 export const NoHeader: Story = {
-  decorators: [ThemeSwitcherDecorator],
-  render: () => (
-    <div className="bg-accent p-12 max-sm:p-6">
-      <Dialog>
-        <Dialog.Content>{loremIpsum}</Dialog.Content>
-        <Dialog.Footer>
-          <Button variant="secondary">Secondary</Button>
-        </Dialog.Footer>
-      </Dialog>
-    </div>
-  ),
+	decorators: [ThemeSwitcherDecorator],
+	render: () => (
+		<div className="bg-accent p-12 max-sm:p-6">
+			<Dialog>
+				<Dialog.Content>{loremIpsum}</Dialog.Content>
+				<Dialog.Footer>
+					<Button variant="secondary">Secondary</Button>
+				</Dialog.Footer>
+			</Dialog>
+		</div>
+	),
 };
--- lib/ui/Dialog/Dialog.tsx
+++ lib/ui/Dialog/Dialog.tsx
@@ -4,89 +4,121 @@ import { cn } from "@/utils";
 import { Separator } from "../separator";
 
 export interface DialogProps {
-  children: React.ReactNode;
-  className?: string;
-  asChild?: boolean;
+	children: React.ReactNode;
+	className?: string;
+	asChild?: boolean;
 }
 
 const Dialog: React.FC<DialogProps> = ({ className, children, asChild }) => {
-  const Comp = asChild ? Slot : "div";
-  return (
-    <Comp className={cn(`flex flex-col w-full h-full bg-card pb-3 rounded-lg border-1`, className)}>{children}</Comp>
-  );
+	const Comp = asChild ? Slot : "div";
+	return (
+		<Comp
+			className={cn(
+				`flex flex-col w-full h-full bg-card pb-3 rounded-lg border-1`,
+				className,
+			)}
+		>
+			{children}
+		</Comp>
+	);
 };
 
 const DialogContent = ({
-  children,
-  className,
-  asChild,
+	children,
+	className,
+	asChild,
 }: {
-  children: React.ReactNode;
-  className?: string;
-  asChild?: boolean;
+	children: React.ReactNode;
+	className?: string;
+	asChild?: boolean;
 }) => {
-  const Comp = asChild ? Slot : "div";
+	const Comp = asChild ? Slot : "div";
 
-  return (
-    <Comp className={cn(`flex flex-col px-7 pt-[30px] h-full overflow-auto text-foreground text-sm`, className)}>
-      {children}
-    </Comp>
-  );
+	return (
+		<Comp
+			className={cn(
+				`flex flex-col px-7 pt-[30px] h-full overflow-auto text-secondary-foreground text-sm`,
+				className,
+			)}
+		>
+			{children}
+		</Comp>
+	);
 };
 
 const DialogFooter = ({
-  children,
-  className,
-  asChild,
-  noSeparator,
+	children,
+	className,
+	asChild,
+	noSeparator,
 }: {
-  children: React.ReactNode;
-  className?: string;
-  asChild?: boolean;
-  noSeparator?: boolean;
+	children: React.ReactNode;
+	className?: string;
+	asChild?: boolean;
+	noSeparator?: boolean;
 }) => {
-  const Comp = asChild ? Slot : "div";
-  return (
-    <>
-      {!noSeparator && (
-        <div className="px-5 mt-[30px]">
-          <Separator />
-        </div>
-      )}
-      <Comp className={cn("m-6 mb-7 flex justify-end", className)}>{children}</Comp>
-    </>
-  );
+	const Comp = asChild ? Slot : "div";
+	return (
+		<>
+			{!noSeparator && (
+				<div className="px-5 mt-[30px]">
+					<Separator />
+				</div>
+			)}
+			<Comp className={cn("m-6 mb-7 flex justify-end", className)}>
+				{children}
+			</Comp>
+		</>
+	);
 };
 
 const DialogHeader = ({
-  children,
-  className,
-  asChild,
+	children,
+	className,
+	asChild,
+	variant = "normal",
+	size = "md",
 }: {
-  children: React.ReactNode;
-  className?: string;
-  asChild?: boolean;
+	children: React.ReactNode;
+	className?: string;
+	asChild?: boolean;
+	variant?: "brand" | "normal";
+	size?: "md" | "sm";
 }) => {
-  const Comp = asChild ? Slot : "div";
-  return (
-    <Comp
-      className={cn(
-        "sm:mb-4 bg-brand-dark dark:bg-brand/65 text-white text-xs font-light px-3 pt-3 pb-2 rounded-t-lg",
-        className,
-      )}
-    >
-      {children}
-    </Comp>
-  );
+	const Comp = asChild ? Slot : "div";
+
+	const variantClasses = {
+		brand: "bg-brand-dark dark:bg-brand/65 text-white",
+		normal: "bg-title text-title-foreground",
+	};
+
+	const sizeClasses = {
+		md: "px-5 pt-4 pb-3 text-sm font-medium",
+		sm: "px-3 pt-3 pb-2 text-xs",
+	};
+
+	return (
+		<Comp
+			className={cn(
+				variantClasses[variant],
+				sizeClasses[size],
+				"sm:mb-4  rounded-t-lg",
+				className,
+			)}
+		>
+			{children}
+		</Comp>
+	);
 };
 
 export interface CompoundComponents {
-  Content: typeof DialogContent;
-  Footer: typeof DialogFooter;
-  Header: typeof DialogHeader;
+	Content: typeof DialogContent;
+	Footer: typeof DialogFooter;
+	Header: typeof DialogHeader;
 }
 
-const DialogWithCompoundComponents = Dialog as React.FC<DialogProps> & CompoundComponents;
+const DialogWithCompoundComponents = Dialog as React.FC<DialogProps> &
+	CompoundComponents;
 
 DialogWithCompoundComponents.displayName = "Dialog";
 
