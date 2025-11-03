74a7294222e738d9e8114c28aebb71f25a15c12d
Wojciech Piątkowski <chmurson@gmail.com>
2025-11-03 21:03:53 +0100

    feat: export textarea and more colors in stories

--- lib/ui/Textarea/Textarea.tsx
+++ lib/ui/Textarea/Textarea.tsx
@@ -1,8 +1,6 @@
 import { cn } from "@/utils";
 import * as React from "react";
 
-// TODO [ToDr] Remove completely / use shared ui.
-
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
@@ -10,7 +8,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ classNa
   return (
     <textarea
       className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       ref={ref}
